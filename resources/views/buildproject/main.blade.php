a5f3df3c73a3a21cd7667546e0558214bab3d867
Phont <nguyentrongpho@gmail.com>
2022-05-11 16:19:00 +0700

    update

--- resources/views/buildproject/main.blade.php
+++ resources/views/buildproject/main.blade.php
@@ -69,7 +69,7 @@
 	</div>
 	<div class="card-footer">
 		<a href="#" onclick="save()"  class="btn btn-primary float-left mr-2">
-			<i class="fa fa-floppy-o"></i> @lang('Save & Close')
+			<i class="fas fa-save"></i> @lang('Save & Close')
 		</a>
 		<a href="#" onclick="update()"  class="btn btn-warning float-left">
 			<i class="fa fa-wrench"></i> @lang('Update')
